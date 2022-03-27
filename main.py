d2e4748666769a2cbf859a11a09fc6230e209d27
Moran <morika.b@gmail.com>
2022-03-27 15:44:54 +0300

    WIP

--- main.py
+++ main.py
@@ -33,19 +33,7 @@ class CommuniqueApp(object):
         upload_wanted_hosts.pack(side=TOP)
 
         self.wanted_hosts_grid = Frame(self.wanted_hosts_frame)
-        self.hosts = []
-
-        # self.feet = StringVar()
-        # feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
-        # feet_entry.grid(column=2, row=1, sticky=(W, E))
-        #
-        # self.meters = StringVar()
-        # ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
-        #
-        # ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
-
-        # feet_entry.focus()
-        # root.bind("<Return>", self.calculate)
+        self.wanted_hosts_count = 0
         self.input_dict = {"organisms": {}}
 
         # TODO - after running "optimize", we need to regenerate the dictionary for the modules code
@@ -69,12 +57,17 @@ class CommuniqueApp(object):
         ttk.Label(self.wanted_hosts_grid, text="optimization priority").grid(column=2, row=0)
         ttk.Label(self.wanted_hosts_grid, text="expression levels path (optional)").grid(column=3, row=0)
 
-        for index, genome_path in enumerate(hosts_files):
-            row = index + 1
+        initial_row = 1
+        if self.wanted_hosts_count > 0:
+            # TODO - handle duplicates
+            initial_row = self.wanted_hosts_count + 1
 
-            host_name = StringVar()
-            host_name.set(os.path.basename(genome_path))
-            ttk.Entry(self.wanted_hosts_grid, textvariable=host_name).grid(column=0, row=row)
+        for index, genome_path in enumerate(hosts_files):
+            row = initial_row + index
+
+            host_name_var = StringVar()
+            host_name_var.set(os.path.basename(genome_path))
+            ttk.Entry(self.wanted_hosts_grid, textvariable=host_name_var).grid(column=0, row=row)
 
             genome_path_var = StringVar()
             genome_path_var.set(genome_path)
@@ -85,31 +78,52 @@ class CommuniqueApp(object):
             optimization_priority.set(50)
             ttk.Spinbox(self.wanted_hosts_grid, from_=1, to=100, textvariable=optimization_priority).grid(column=2,
                                                                                                           row=row)
-            ttk.Button(self.wanted_hosts_grid, text="remove", command=self.remove_host).grid(column=4, row=row)
+
+            expression_level_button = ttk.Button(self.wanted_hosts_grid, text="upload")
+            expression_level_button.grid(column=3, row=row)
+            expression_level_button.bind("<Button-1>", self.upload_expression)
+
+            remove_button = ttk.Button(self.wanted_hosts_grid, text="remove")
+            remove_button.grid(column=4, row=row)
+            remove_button.bind("<Button-1>", self.remove_host)
 
             organism = {
-                "host_name": host_name,
+                "host_name": host_name_var,
                 "genome_path": genome_path_var,
                 "optimized": True,
-                "expression_csv": None,  # TODO - fix this
+                "expression_csv": None,
                 "optimization_priority": optimization_priority,
             }
             organisms[genome_path] = organism
 
         for child in self.wanted_hosts_grid.winfo_children():
             child.grid_configure(padx=5, pady=5)
-
         self.wanted_hosts_grid.pack()
 
-    def remove_host(self):
-        # TODO - add logic
-        pass
+        self.wanted_hosts_count += len(hosts_files)
 
-    @staticmethod
-    def clear_frame(frame: Frame) -> None:
-        # destroy all widgets from frame
-        for widget in frame.winfo_children():
-            widget.destroy()
+    def upload_expression(self, event) -> None:
+        expression_file_name = filedialog.askopenfilename(filetypes=[("csv", "*.csv")])
+        upload_widget = event.widget
+        host_row = upload_widget.grid_info()["row"]
+        # TODO - define consts for column numbers
+        genome_path = self.wanted_hosts_grid.grid_slaves(row=host_row, column=1)[0]
+        host_entry = self.input_dict["organisms"][genome_path]
+        host_entry["expression_csv"] = expression_file_name
+
+    def remove_host(self, event) -> None:
+        number_of_columns = 5
+        remove_widget = event.widget
+        row_to_remove = remove_widget.grid_info()["row"]
+        remove_widget.grid_remove()
+        for i in range(row_to_remove, self.wanted_hosts_count+1):
+            if i == self.wanted_hosts_count:
+                for j in range(number_of_columns-1):
+                    self.wanted_hosts_grid.grid_slaves(row=i, column=j)[0].grid_remove()
+            else:
+                for j in range(number_of_columns):
+                    self.wanted_hosts_grid.grid_slaves(row=i, column=j)[0].grid(row=i-1, column=j)
+        self.wanted_hosts_count -= 1
 
 
 if __name__ == '__main__':
