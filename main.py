2a77d35cbe40aea1c2ec4a012c9600432a17ee6b
Moran <morika.b@gmail.com>
2022-03-28 17:30:01 +0300

    Add basic advanced options functionality

--- main.py
+++ main.py
@@ -10,20 +10,21 @@ class CommuniqueApp(object):
     def __init__(self, master: Tk) -> None:
         master.title("Communique")
 
-        mainframe = ttk.Frame(master, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.mainframe = ttk.Frame(master, padding="3 3 12 12")
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         master.columnconfigure(0, weight=1)
         master.rowconfigure(0, weight=1)
         master.geometry("700x1000")  # Set window size
 
         # Scroll Bar
-        self.scrollbar = ttk.Scrollbar(mainframe, orient="vertical")
+        # TODO - fix it to make it scrollable for real
+        self.scrollbar = ttk.Scrollbar(self.mainframe, orient="vertical")
         self.scrollbar.pack(side="right", fill="y")
 
         #########################
         # Sequence to optimize
         #########################
-        self.sequence_label_frame = ttk.Labelframe(mainframe, text="Sequence to Optimize")
+        self.sequence_label_frame = ttk.Labelframe(self.mainframe, text="Sequence to Optimize")
         self.sequence_label_frame.pack(fill="both", expand="yes")
         upload_sequence = ttk.Button(self.sequence_label_frame, text="Upload Sequence", command=self.upload_sequence)
         upload_sequence.pack(side=TOP)
@@ -34,7 +35,7 @@ class CommuniqueApp(object):
         #########################
         # Wanted hosts
         #########################
-        self.wanted_hosts_frame = ttk.Labelframe(mainframe, text="Wanted Hosts")
+        self.wanted_hosts_frame = ttk.Labelframe(self.mainframe, text="Wanted Hosts")
         self.wanted_hosts_frame.pack(fill="both", expand="yes")
         upload_wanted_hosts = ttk.Button(self.wanted_hosts_frame,
                                          text="Upload .gb files",
@@ -45,7 +46,7 @@ class CommuniqueApp(object):
         #########################
         # Unwanted hosts
         #########################
-        self.unwanted_hosts_frame = ttk.Labelframe(mainframe, text="Unwanted Hosts")
+        self.unwanted_hosts_frame = ttk.Labelframe(self.mainframe, text="Unwanted Hosts")
         self.unwanted_hosts_frame.pack(fill="both", expand="yes")
         upload_unwanted_hosts = ttk.Button(self.unwanted_hosts_frame,
                                            text="Upload .gb files",
@@ -53,13 +54,24 @@ class CommuniqueApp(object):
         upload_unwanted_hosts.pack(side=TOP)
         self.unwanted_hosts_grid = Frame(self.unwanted_hosts_frame)
 
+        # TODO - think again about the location of the buttons (right/left/center)
+        bottom_frame = ttk.Frame(self.mainframe)
+        bottom_frame.pack(side=BOTTOM, pady=20)
+
+        # Advanced Options
+        self.options_button = ttk.Button(bottom_frame, text="Advanced Options", command=self.advanced_options)
+        self.options_button.grid(row=0, column=0)
+
         # Optimize Button
-        self.optimize_button = ttk.Button(mainframe, text="Optimize", command=self.optimize)
-        self.optimize_button.pack(side=BOTTOM, pady=20)
+        self.optimize_button = ttk.Button(bottom_frame, text="Optimize", command=self.optimize)
+        self.optimize_button.grid(row=0, column=1)
 
         # User Input Parameters
         self.organisms = {}
         self.sequence = None
+
+        self.tuning_parameter = IntVar()
+        self.tuning_parameter.set(50)       # TODO - move to constant
 
     def get_hosts_count(self, is_optimized: bool) -> int:
         return len({key: value for key, value in self.organisms.items() if value["optimized"] == is_optimized})
@@ -104,7 +116,6 @@ class CommuniqueApp(object):
             ttk.Entry(grid, textvariable=genome_path_var).grid(column=1, row=row)
             optimization_priority_var = IntVar()
             optimization_priority_var.set(50)
-            # TODO - recheck from and to parameters here.
             ttk.Spinbox(grid, from_=1, to=100, textvariable=optimization_priority_var).grid(column=2, row=row)
 
             self.create_upload_expression_frame(grid=grid, row=row)
@@ -193,6 +204,18 @@ class CommuniqueApp(object):
                 self.wanted_hosts_grid = Frame(self.wanted_hosts_frame)
             else:
                 self.unwanted_hosts_grid = Frame(self.unwanted_hosts_frame)
+
+    def advanced_options(self) -> None:
+        options_window = Toplevel(self.mainframe)
+        options_window.title("Advanced Options")
+        options_window.geometry("300x200")
+
+        options_frame = ttk.Frame(options_window, padding="3 3 12 12")
+        ttk.Label(options_frame, text="Tuning Parameter: ").grid(row=0, column=0)
+        ttk.Spinbox(options_frame, from_=1, to=100, textvariable=self.tuning_parameter).grid(row=0, column=1)
+        # TODO - add clustering number (talk with Liyam)
+
+        options_frame.pack()
 
     def optimize(self) -> None:
         # TODO - create dict, run modules and display the final window
