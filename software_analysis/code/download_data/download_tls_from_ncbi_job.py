a3c00344987580a133ab1f1a6bb66dd1a958f49b
leviliyam <liyam@gmail.com>
2022-07-27 19:13:35 +0300

    fixed download files

--- software_analysis/code/download_data/download_tls_from_ncbi_job.py
+++ software_analysis/code/download_data/download_tls_from_ncbi_job.py
@@ -29,7 +29,7 @@ def download_files():
         line = line.strip()
         if "fsa_nt.gz" in line or ".mstr.gbff.gz" in line:
             f_name = line.split('"')[1]
-            command = 'wget ' + ' https://ftp.ncbi.nlm.nih.gov/genbank/tls/K/' +f_name +' --no-check-certificate'
+            command = 'wget -P ' + destination_dir +  ' https://ftp.ncbi.nlm.nih.gov/genbank/tls/K/' +f_name +' --no-check-certificate'
             run_cmd(command)
             print(command)
             # destination_file = destination_dir + '/' + f_name
