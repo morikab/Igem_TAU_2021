9e592b96ea215907b685c74e45320b8434263f45
leviliyam <liyam@gmail.com>
2022-07-27 19:34:06 +0300

    fixed download files

--- software_analysis/code/download_data/download_genomes_from_ncbi_job.py
+++ software_analysis/code/download_data/download_genomes_from_ncbi_job.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import subprocess
 
 destination_dir = '../../data/'
@@ -48,41 +49,3 @@ if __name__ == "__main__":
 
 
 
-
-
-# destination_dir = '/../../data/'
-#
-# def run_cmd(cmd, verbose=False, *args, **kwargs):
-#     process = subprocess.Popen(
-#         cmd,
-#         stdout=subprocess.PIPE,
-#         stderr=subprocess.PIPE,
-#         shell=True
-#     )
-#     std_out, std_err = process.communicate()
-#     if verbose:
-#         print(std_out.strip(), std_err)
-#
-#
-# def download_files():
-#     with open("cds_files.txt", "r") as cds_file:
-#         file_names = cds_file.readlines()
-#         for file_name in file_names:
-#             # cds_destination_file = destination_dir+'ncbi_genome_cds'
-#             run_cmd("wget" +  file_name.strip() + " -P ncbi_cds")
-#             run_cmd("gzip -d ncbi_cds/" + file_name.strip() )
-#
-#     with open("rna_files.txt", "r") as rna_files:
-#         file_names = rna_files.readlines()
-#         for file_name in file_names:
-#             # rna_destination_file = destination_dir+'ncbi_genome_rna'
-#             run_cmd("wget" +  file_name.strip() + " -P ncbi_rna")
-#             run_cmd("gzip -d ncbi_rna/" + file_name.strip() )
-#
-#
-#
-# if __name__ == "__main__":
-#     print("Start")
-#     download_files()
-#     print("The end")
-#
