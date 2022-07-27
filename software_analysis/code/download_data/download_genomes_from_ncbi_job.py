9620938a827150e79ac90e116b75b31834141531
leviliyam <liyam@gmail.com>
2022-07-27 19:44:50 +0300

    fixed download files

--- software_analysis/code/download_data/download_genomes_from_ncbi_job.py
+++ software_analysis/code/download_data/download_genomes_from_ncbi_job.py
@@ -20,9 +20,7 @@ def download_files():
         file_names = cds_file.readlines()
         for file_name in file_names:
             cds_destination_file = destination_dir+'ncbi_genome_cds'
-            # command = 'wget ‐P ' + cds_destination_file + ' ' + file_name.strip()
             command = 'wget -P ' + cds_destination_file +  ' ' + file_name.strip() +' --no-check-certificate'
-
             run_cmd(command)
             print(command)
         command = 'gzip -d ' + cds_destination_file + '/*'
@@ -33,16 +31,12 @@ def download_files():
         file_names = rna_files.readlines()
         for file_name in file_names:
             rna_destination_file = destination_dir+'ncbi_genome_rna'
-            command = 'wget -P ' + rna_destination_file +' ' + file_name.strip()
+            command = 'wget -P ' + rna_destination_file +  ' ' + file_name.strip() +' --no-check-certificate'
             run_cmd(command)
             print(command)
         command = 'gzip -d ' +  rna_destination_file +'/*'
         print(command)
         run_cmd(command)
-
-# wget -P ../../data/genbank_tls https://ftp.ncbi.nlm.nih.gov/genbank/tls/K/tls.KACJ.1.fsa_nt.gz --no-check-certificate
-# gzip -d ../../data/genbank_tls/tls.KACJ.1.fsa_nt.gz
-# wget ‐P ../../data/ncbi_genome_cds https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/014/151/665/GCF_014151665.1_ASM1415166v1/GCF_014151665.1_ASM1415166v1_cds_from_genomic.fna.gz
 
 if __name__ == "__main__":
     print("Start")
