fb959399924cd826f8d8cf70f0897856bf50b6c4
leviliyam <liyam@gmail.com>
2022-07-30 13:21:14 +0300

    fixed download files

--- software_analysis/code/download_data/download_genomes_from_ncbi_job.py
+++ software_analysis/code/download_data/download_genomes_from_ncbi_job.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import subprocess
 
 destination_dir = '../../data/refseq_genomes/'
@@ -16,27 +17,38 @@ def run_cmd(cmd, verbose=False, *args, **kwargs):
 
 
 def download_files():
-    # with open("cds_files.txt", "r") as cds_file:
-    #     file_names = cds_file.readlines()
-    #     for file_name in file_names:
-    #         cds_destination_file = destination_dir+'ncbi_genome_cds'
-    #         command = 'wget -P ' + cds_destination_file +  ' ' + file_name.strip() +' --no-check-certificate'
-    #         run_cmd(command)
-    #         print(command)
-    #     command = 'gzip -d ' + cds_destination_file + '/*'
-    #     print(command)
-    #     run_cmd(command)
-
     with open("rna_files.txt", "r") as rna_files:
         file_names = rna_files.readlines()
-        for file_name in file_names:
+        for file_full_name in file_names:
+            f_name = file_full_name.strip().split('/')[-1]
             rna_destination_file = destination_dir+'ncbi_genome_rna'
-            command = 'wget -P ' + rna_destination_file +  ' ' + file_name.strip() +' --no-check-certificate'
-            run_cmd(command)
-            print(command)
-        command = 'gzip -d ' +  rna_destination_file +'/*'
-        print(command)
-        run_cmd(command)
+            downloaded_files = os.listdir(rna_destination_file)
+            if f_name not in downloaded_files:
+                if f_name[:-3] not in downloaded_files:
+                    command = 'wget -P ' + rna_destination_file +  ' ' + file_full_name.strip() +' --no-check-certificate'
+                    run_cmd(command)
+                    print(command)
+                command = 'gzip -d ' + rna_destination_file + '/'+  f_name
+                run_cmd(command)
+                print(command)
+                continue
+
+    with open("cds_files.txt", "r") as cds_file:
+        file_names = cds_file.readlines()
+        for file_full_name in file_names:
+            f_name = file_full_name.strip().split('/')[-1]
+            cds_destination_file = destination_dir+'ncbi_genome_cds'
+            downloaded_files = os.listdir(cds_destination_file)
+            if f_name not in downloaded_files:
+                if f_name[:-3] not in downloaded_files:
+                    command = 'wget -P ' + cds_destination_file +  ' ' + file_full_name.strip() +' --no-check-certificate'
+                    run_cmd(command)
+                    print(command)
+                command = 'gzip -d ' + cds_destination_file + '/' + f_name
+                run_cmd(command)
+                print(command)
+                continue
+
 
 if __name__ == "__main__":
     print("Start")
