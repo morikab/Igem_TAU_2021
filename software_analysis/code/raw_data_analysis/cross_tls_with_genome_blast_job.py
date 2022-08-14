5c521d045a75a2ba7c3a79159e72d5556c5eaa2e
leviliyam <liyam@gmail.com>
2022-08-14 09:00:44 +0300

    cross genomes into job factory

--- software_analysis/code/raw_data_analysis/cross_tls_with_genome_blast_job.py
+++ software_analysis/code/raw_data_analysis/cross_tls_with_genome_blast_job.py
@@ -24,7 +24,7 @@ def run_cmd(cmd, verbose=False, *args, **kwargs):
 def blastn_run(tls_inp):
     blastn_loc = '/tamir1/liyamlevi/tools/ncbi-blast-2.11.0+/bin/blastn'
     db_loc = '../../data/processed_genomes/blast_db/16s_genomes'
-    other_preferences = ' -subject_besthit -outfmt 10 -max_target_seqs 1 -num_threads 4'
+    other_preferences = ' -subject_besthit -outfmt 10 -max_target_seqs 1 -num_threads 1'
     tls_output = tls_inp[:-6]+'csv'
     command = blastn_loc + ' -db ' + db_loc + ' -query ' + tls_inp + ' -out ' + tls_output + other_preferences
     # run_cmd(command)
@@ -54,17 +54,46 @@ def run_all_tls(metadata_fid ):
 # run_all_tls(metadata_fid = '../../data/processed_tls/tls_assembly_metadata.csv')
 
 
+def write_job(lines, job_fid):
+    f = open(job_fid, 'w')
+    f.write(
+        '#!/bin/sh \n cd /tamir1/liyamlevi/projects/communique/Igem_TAU_2021/software_analysis/code/raw_data_analysis\n')
+    for line in lines:
+        f.write(line + '\n')
+    f.close()
 
+
+def filename_to_sent_job(sh_file):
+    send_prefix = 'qsub -q TullerNano -r y '
+    send_suffix = ' -l cput=01:00:00,pmem=12gb,mem=10gb,pvmem=1gb,vmem=1gb '
+    error_file = sh_file[:-3] + '_error.txt'
+    output_file = sh_file[:-3] + '_output.txt'
+    line = send_prefix + ' -e ' + error_file + ' -o ' + output_file + send_suffix
+    return line
 
 
 if __name__ == "__main__":
     print('Start')
     command_list, df = run_all_tls(metadata_fid = '../../data/processed_tls/tls_assembly_metadata.csv')
-    for command in command_list:
-        proc = subprocess.Popen(command, shell=True,
-                                stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        out, err = proc.communicate()
-        print("{} : {}".format(command, out.decode()))
+
+    job_files = []
+    for idx, command in enumerate(command_list):
+        filename = str(idx) + '_blast_job.sh'
+        job_files.append(filename)
+        write_job([command], 'jobs/' + filename)
+
+
+    master_commands = [filename_to_sent_job(sh_file) for sh_file in job_files]
+    f = open('jobs/mstr_job.sh', 'w')
+    f.write(
+        '#!/bin/sh \n')
+    for line in master_commands:
+        f.write(line + '\n')
+    f.close()
+        # proc = subprocess.Popen(command, shell=True,
+        #                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        # out, err = proc.communicate()
+        # print("{} : {}".format(command, out.decode()))
     print("The end")
 
 
