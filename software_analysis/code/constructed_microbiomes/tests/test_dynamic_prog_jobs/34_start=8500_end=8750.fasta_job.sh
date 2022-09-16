53b8e560a3c8869e6534d4141a8868737f4caa05
leviliyam <liyam@gmail.com>
2022-09-16 11:59:13 +0300

    added jobs and split 16s files

---
+++ software_analysis/code/constructed_microbiomes/tests/test_dynamic_prog_jobs/34_start=8500_end=8750.fasta_job.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+ cd /tamir1/liyamlevi/projects/communique/Igem_TAU_2021/software_analysis/code/constructed_microbiomes/tests
+ module load Python3.9Plus
+python -c from pairwise_alignment_test import align_seq_to_fasta;  pairwise_alignment_test.align_seq_to_fasta('../../../data/tested_results/KDVY_example_metagenome/KDVY_first_entry_KDVY01000001.1.fasta', '../../../data/tested_results/genomes_16s_sliced/34_start=8500_end=8750.fasta')
