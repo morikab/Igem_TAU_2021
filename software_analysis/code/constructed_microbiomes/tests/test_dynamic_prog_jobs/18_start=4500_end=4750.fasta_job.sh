aad8babce79252f3009b887150b1589584eadd1b
leviliyam <liyam@gmail.com>
2022-09-26 10:25:18 -0400

    deleted previous job files

--- software_analysis/code/constructed_microbiomes/tests/test_dynamic_prog_jobs/18_start=4500_end=4750.fasta_job.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
- cd /tamir1/liyamlevi/projects/communique/Igem_TAU_2021/software_analysis/code/constructed_microbiomes/tests
- module load Python3.9Plus
-python -c "from pairwise_align_function.py import align_seq_to_fasta; pairwise_align_function.align_seq_to_fasta('../../../data/tested_results/KDVY_example_metagenome/KDVY_first_entry_KDVY01000001.1.fasta', '../../../data/tested_results/genomes_16s_sliced/18_start=4500_end=4750.fasta')"
