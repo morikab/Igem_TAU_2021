94a1a69a069e97535635c613515bee23749e7485
leviliyam <liyam@gmail.com>
2022-09-26 11:31:28 -0400

    fixed master file for dynamic programing job to run all jobs in the directory

--- software_analysis/code/constructed_microbiomes/tests/test_KDVY_first_entry_execs_mstr.sh
+++ software_analysis/code/constructed_microbiomes/tests/test_KDVY_first_entry_execs_mstr.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /tamir1/liyamlevi/projects/communique/Igem_TAU_2021/software_analysis/code/constructed_microbiomes/tests/test_local_align_KDVY_first_entry
 chmod 777 ./*
-for file in ./*; do   ./"$file" ; done
+
+for file in ./*; do  qsub -q TullerNano -r y  -e 0_blast_job_error.txt -o 0_blast_job_output.txt -l cput=01:00:00,pmem=1gb,mem=1gb,pvmem=1gb,vmem=1gb "$file"  echo "$file"; done
