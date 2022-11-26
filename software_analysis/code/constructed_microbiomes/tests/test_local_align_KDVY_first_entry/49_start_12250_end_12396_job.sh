eb771626b21338d367321958460e925093ab279b
leviliyam <leviliyam@gmail.com>
2022-11-26 23:43:08 +0200

    following error correction I re splitted the 16s rrna's from genome and fixed the job files to fit the new fasta files.

---
+++ software_analysis/code/constructed_microbiomes/tests/test_local_align_KDVY_first_entry/49_start_12250_end_12396_job.sh
@@ -0,0 +1,4 @@
+# !/bin/sh
+cd /tamir1/liyamlevi/projects/communique/Igem_TAU_2021/software_analysis/code/constructed_microbiomes/tests/test_local_align_KDVY_first_entry
+module load Python3.9Plus
+python 49_start_12250_end_12396_exec.py
