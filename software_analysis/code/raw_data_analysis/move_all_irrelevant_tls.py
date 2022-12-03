4f33efd0333523bd57039731220c0abc5adedded
leviliyam <leviliyam@gmail.com>
2022-12-03 15:32:21 +0200

    filtering tls files (I had to do this a very long time ago)

--- software_analysis/code/raw_data_analysis/move_all_irrelevant_tls.py
+++ software_analysis/code/raw_data_analysis/move_all_irrelevant_tls.py
@@ -10,6 +10,6 @@ non_needed_files = [f for f in tls_files if f[:8] not in tls_files]
 
 print('mkdir non_16s_tls_data')
 for f in non_needed_files:
-    print(f'mv {f} non_16s_tls_data')
+    print('mv ', f, ' non_16s_tls_data')
 
 
