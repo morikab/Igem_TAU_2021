ccefbf0fd6feee80768b1372b02771c238d5d8be
leviliyam <leviliyam@gmail.com>
2022-12-03 15:34:45 +0200

    filtering tls files (I had to do this a very long time ago)

--- software_analysis/code/raw_data_analysis/move_all_irrelevant_tls.py
+++ software_analysis/code/raw_data_analysis/move_all_irrelevant_tls.py
@@ -1,15 +1,27 @@
 ### moves all tls projects that are not 16s projects into a designated file
 from os import listdir
 from os.path import isfile, join
+import subprocess
 
 tls_blast_path = '../../data/genbank_tls/'
+
+def run_cmd(cmd, verbose=False, *args, **kwargs):
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True
+    )
+    std_out, std_err = process.communicate()
+    if verbose:
+        print(std_out.strip(), std_err)
 
 tls_files = [f for f in listdir(tls_blast_path) if isfile(join(tls_blast_path, f))]
 tls_csv_file_names = [f[:8] for f in tls_files if '.csv' if f]
 non_needed_files = [f for f in tls_files if f[:8] not in tls_files]
 
-print('mkdir non_16s_tls_data')
+run_cmd('mkdir non_16s_tls_data')
 for f in non_needed_files:
-    print('mv ', f, ' non_16s_tls_data')
+    run_cmd('mv '+ f +' non_16s_tls_data')
 
 
