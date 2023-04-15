9c456825bc0158d8ff3c16e984feb0aa6fefbcca
Moran <morika.b@gmail.com>
2023-04-15 22:35:45 +0300

    * Complete summary xslx file creation.

--- analysis/orf_model_analysis/generate_summary_file.py
+++ analysis/orf_model_analysis/generate_summary_file.py
@@ -1,24 +1,105 @@
 import json
 import os
 
+import openpyxl
+
+
+aa_to_cell_column = {
+    "C": "L",
+    "D": "M",
+    "S": "N",
+    "Q": "O",
+    "M": "P",
+    "N": "Q",
+    "P": "R",
+    "K": "S",
+    "_": "T",
+    "T": "U",
+    "F": "V",
+    "A": "W",
+    "G": "X",
+    "I": "Y",
+    "L": "Z",
+    "H": "AA",
+    "R": "AB",
+    "W": "AC",
+    "V": "AD",
+    "E": "AE",
+    "Y": "AF",
+}
+
 
 def generate_summary():
     base_directory = "results"
     filename = "run_summary.json"
 
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+
+    initialize_column_headers(worksheet)
+
+    row_offset = 2
     for root, dirs, files in os.walk(base_directory):
         for file in files:
             if file == filename:
                 filepath = os.path.join(root, file)
-                parse_summary_file(filepath)
+                parse_summary_file(file_path=filepath, worksheet=worksheet, row_offset=row_offset)
+                row_offset += 1
+
+    workbook.save("summary.xlsx")
 
 
-def parse_summary_file(file_path: str) -> None:
+def initialize_column_headers(worksheet) -> None:
+    worksheet["A1"] = "optimization_method"
+
+    worksheet["B1"] = "is_ecoli_wanted"
+    worksheet["C1"] = "initial_gene_cai_score_ecoli"
+    worksheet["D1"] = "final_gene_cai_score_ecoli"
+    worksheet["E1"] = "ecoli_dist_score"
+
+    worksheet["F1"] = "is_bacillus_wanted"
+    worksheet["G1"] = "initial_gene_cai_score_bacillus"
+    worksheet["H1"] = "final_gene_cai_score_bacillus"
+    worksheet["I1"] = "bacillus_dist_score"
+
+    worksheet["J1"] = "final_average_distance_score"
+    worksheet["K1"] = "final_weakest_link_score"
+
+    for aa, cell in aa_to_cell_column.items():
+        worksheet[F"{cell}1"] = aa
+
+
+def parse_summary_file(file_path: str, worksheet, row_offset: int) -> None:
     with open(file_path, "r") as summary_file:
         summary = json.load(summary_file)
-        print(summary["evaluation"])
 
-        # TODO - continue from here: * create a row for each amino-acid with the chosen codon
+        organisms = summary["evaluation"]["organisms"]
+        first_organism = organisms[0]
+        if first_organism["name"] == "Escherichia coli":
+            e_coli = organisms[0]
+            bacillus = organisms[1]
+        else:
+            e_coli = organisms[1]
+            bacillus = organisms[0]
+
+        worksheet[f"A{row_offset}"] = summary["user_input"]["optimization_method"]
+
+        worksheet[f"B{row_offset}"] = e_coli["is_wanted"]
+        worksheet[f"C{row_offset}"] = e_coli["cai_initial_score"]
+        worksheet[f"D{row_offset}"] = e_coli["cai_final_score"]
+        worksheet[f"E{row_offset}"] = e_coli["dist_score"]
+
+        worksheet[f"F{row_offset}"] = bacillus["is_wanted"]
+        worksheet[f"G{row_offset}"] = bacillus["cai_initial_score"]
+        worksheet[f"H{row_offset}"] = bacillus["cai_final_score"]
+        worksheet[f"I{row_offset}"] = bacillus["dist_score"]
+
+        worksheet[f"J{row_offset}"] = summary["evaluation"]["average_distance_score"]
+        worksheet[f"K{row_offset}"] = summary["evaluation"]["weakest_link_score"]
+
+        aa_to_optimal_codon = summary["orf"]["aa_to_optimal_codon"]
+        for aa, cell in aa_to_cell_column.items():
+            worksheet[F"{cell}{row_offset}"] = aa_to_optimal_codon.get(aa) or ""
 
 
 if __name__ == "__main__":
